1f9f3b72de033803766ea83dd98515afbf5e42b3
Matheus Eduardo dos Santos <matheusthja1@gmail.com>
2024-04-18 06:57:36 -0300

    migrei parte da tela de inicio pro angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'unicursos';
+
+  constructor(private renderer:Renderer2){}
+
+  toggleTheme() {
+    const body = document.getElementById("body")!;
+    const lightTheme = "lightTheme";
+    const darkTheme = "darkTheme";
+  
+    if (body.classList.contains(lightTheme)) {
+      body.classList.remove(lightTheme);
+      body.classList.add(darkTheme);
+  
+      localStorage.setItem("theme", darkTheme);
+    } else {
+      body.classList.remove(darkTheme);
+      body.classList.add(lightTheme);
+  
+      localStorage.setItem("theme", lightTheme);
+    }
+  }
+  
+  ngOnInit(): void {
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+      document.body.classList.add(theme);
+    }
+    else{
+      let body = document.getElementById("body")!;
+      body.classList.add("darkTheme");
+      localStorage.setItem("theme", "darkTheme");
+    }
+  }
 }
